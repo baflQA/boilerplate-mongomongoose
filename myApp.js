3427d680cdf11d9699e29305ad4ff169eed5c4e3
Bartek Florczak <bflorczak@outlook.com>
2022-02-01 20:22:31 +0100

    Rename variable

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 const { Schema } = mongoose;
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const Person = new Schema({
+const personSchema = new Schema({
   name: String,
   age: Number,
   favoriteFoods: [String]
