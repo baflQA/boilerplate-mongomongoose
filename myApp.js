94128bf76b9e267dc6a04c4d12911ba791d4e237
Bartek Florczak <bflorczak@outlook.com>
2022-02-01 20:14:45 +0100

    Add Person schema

--- myApp.js
+++ myApp.js
@@ -1,6 +1,13 @@
 require('dotenv').config();
 var mongoose = require('mongoose');
+const { Schema } = mongoose;
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Person = new Schema({
+  name: String,
+  age: Number,
+  favoriteFoods: [String]
+})
 
 let Person;
 
